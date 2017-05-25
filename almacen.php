947653db6c6f4fceabee520bff4a617ab3fc123a
javiigar9 <javiigarcia9@gmail.com>
2017-05-25 13:28:31 +0200

    almacen

--- almacen.php
+++ almacen.php
@@ -10,11 +10,7 @@
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.0/css/materialize.min.css">
   <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 
-  <script src="https://code.jquery.com/jquery-3.1.1.min.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.0/js/materialize.min.js"></script>
-
-  <script src="js/admin.js"></script>
-  <link rel="stylesheet" href="css/admin.css">
+  <link rel="stylesheet" href="css/almacen.css">
 
   
 </head>
