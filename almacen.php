f025175d4ec719f684c60ff5ef7d7150178f5704
javiigar9 <javiigarcia9@gmail.com>
2017-05-25 13:05:17 +0200

    Add files via upload

---
+++ almacen.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <link rel="icon" href="img/logo-mini.png">
+  <title>Catering manager</title>
+
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.0/css/materialize.min.css">
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+
+  <script src="https://code.jquery.com/jquery-3.1.1.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.0/js/materialize.min.js"></script>
+
+  <script src="js/admin.js"></script>
+  <link rel="stylesheet" href="css/admin.css">
+
+  
+</head>
+
+<body>
+    
+     <div class="navbar-fixed">
+        <nav id="navbar" class="row" role="navigation">
+            <div class="nav-wrapper">
+
+                <ul class="left">
+                <img class="responsive-img logo-mini" src="img/logo-mini-white.jpg">
+                </ul>
+            <ul class="right">
+            <li><a href="logout.php"><i class="material-icons">power_settings_new</i></a></li>
+            </ul>
+        </div>
+        </nav>
+  </div>
+          
+          
+    
+    
+</body>
+
+</html>
