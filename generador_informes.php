05c0767f5a7649bfc8227c87e81adfc82f313a89
Dafer <aferns08@estudiantes.unileon.es>
2017-06-04 00:15:47 +0200

    Update report generator

--- generador_informes.php
+++ generador_informes.php
@@ -3,118 +3,47 @@
   	session_start();
   	Include('connector_db.php');
 	require('fpdf/fpdf.php');
-
-	global $query, $conection, $username;
+    
   	$conection = establecerConexionDB();
 
 	$pdf = new FPDF();
 	$pdf->AddPage();
 
+    
+    printDay("monday", "Lunes");
+    printDay("tuesday", "Martes");
+    printDay("wednesday", "Miércoles");
+    printDay("thursday", "Jueves");
+    printDay("friday", "Viernes");
+	printDay("saturday", "Sábado");
+    printDay("sunday", "Domingo");
 
-	/* Lunes */
-	$pdf->SetFont('Arial', 'B', 16);
-	$pdf->Write(16, "Lunes\n");
-	$pdf->SetFont('Arial','', 16);
-
-    $sentence = "SELECT user, monday_lunch, monday_dinner FROM MENUS WHERE active = 1 AND user IS NOT NULL";
-
-    $query = mysqli_query($conection, $sentence) or die(ERROR_CONSULTA_DB);
-
-    while($choice = mysqli_fetch_array($query)) {
-    	if(! ($choice['monday_dinner'] == "" && $choice['monday_lunch'] == "") ) {
-    		$pdf->Write(14, "" . $choice['user'] . ":\t\t" . $choice['monday_lunch'] . "\t\t-\t\t" . $choice['monday_dinner'] . "\n");    		
-    	}
-    }
-
-    /* Martes */
-    $pdf->SetFont('Arial', 'B', 16);
-	$pdf->Write(16, "Martes\n");
-	$pdf->SetFont('Arial','', 16);
-
-    $sentence = "SELECT user, tuesday_lunch, tuesday_dinner FROM MENUS WHERE active = 1 AND user IS NOT NULL";
-
-    $query = mysqli_query($conection, $sentence) or die(ERROR_CONSULTA_DB);
-
-    while($choice = mysqli_fetch_array($query)) {
-    	if(! ($choice['tuesday_dinner'] == "" && $choice['tuesday_lunch'] == "") ) {
-    		$pdf->Write(14, "" . $choice['user'] . ":\t\t" . $choice['tuesday_lunch'] . "\t\t-\t\t" . $choice['tuesday_dinner'] . "\n");    		
-    	}
-    }
-
-    /* Miércoles */
-    $pdf->SetFont('Arial', 'B', 16);
-	$pdf->Write(16, "Miércoles\n");
-	$pdf->SetFont('Arial','', 16);
-	
-    $sentence = "SELECT user, wednesday_lunch, wednesday_dinner FROM MENUS WHERE active = 1 AND user IS NOT NULL";
-
-    $query = mysqli_query($conection, $sentence) or die(ERROR_CONSULTA_DB);
-
-    while($choice = mysqli_fetch_array($query)) {
-    	if(! ($choice['wednesday_dinner'] == "" && $choice['wednesday_lunch'] == "") ) {
-    		$pdf->Write(14, "" . $choice['user'] . ":\t\t" . $choice['wednesday_lunch'] . "\t\t-\t\t" . $choice['wednesday_dinner'] . "\n");    		
-    	}
-    }
-
-    /* Jueves */
-    $pdf->SetFont('Arial', 'B', 16);
-	$pdf->Write(16, "Jueves\n");
-	$pdf->SetFont('Arial','', 16);
-	
-    $sentence = "SELECT user, thursday_lunch, thursday_dinner FROM MENUS WHERE active = 1 AND user IS NOT NULL";
-
-    $query = mysqli_query($conection, $sentence) or die(ERROR_CONSULTA_DB);
-
-    while($choice = mysqli_fetch_array($query)) {
-    	if(! ($choice['thursday_dinner'] == "" && $choice['thursday_lunch'] == "") ) {
-    		$pdf->Write(14, "" . $choice['user'] . ":\t\t" . $choice['thursday_lunch'] . "\t\t-\t\t" . $choice['thursday_dinner'] . "\n");    		
-    	}
-    }
-
-    /* Viernes */
-    $pdf->SetFont('Arial', 'B', 16);
-	$pdf->Write(16, "Viernes\n");
-	$pdf->SetFont('Arial','', 16);
-	
-    $sentence = "SELECT user, friday_lunch, friday_dinner FROM MENUS WHERE active = 1 AND user IS NOT NULL";
-
-    $query = mysqli_query($conection, $sentence) or die(ERROR_CONSULTA_DB);
-
-    while($choice = mysqli_fetch_array($query)) {
-    	if(! ($choice['friday_dinner'] == "" && $choice['friday_lunch'] == "") ) {
-    		$pdf->Write(14, "" . $choice['user'] . ":\t\t" . $choice['friday_lunch'] . "\t\t-\t\t" . $choice['friday_dinner'] . "\n");    		
-    	}
-    }
-
-    /* Sábado */
-    $pdf->SetFont('Arial', 'B', 16);
-	$pdf->Write(16, "Sábado\n");
-	$pdf->SetFont('Arial','', 16);
-	
-    $sentence = "SELECT user, saturday_lunch, saturday_dinner FROM MENUS WHERE active = 1 AND user IS NOT NULL";
-
-    $query = mysqli_query($conection, $sentence) or die(ERROR_CONSULTA_DB);
-
-    while($choice = mysqli_fetch_array($query)) {
-    	if(! ($choice['saturday_dinner'] == "" && $choice['saturday_lunch'] == "") ) {
-    		$pdf->Write(14, "" . $choice['user'] . ":\t\t" . $choice['saturday_lunch'] . "\t\t-\t\t" . $choice['saturday_dinner'] . "\n");    		
-    	}
-    }
-
-    /* Domingo */
-    $pdf->SetFont('Arial', 'B', 16);
-	$pdf->Write(16, "Domingo\n");
-	$pdf->SetFont('Arial','', 16);
-	
-    $sentence = "SELECT user, sunday_lunch, sunday_dinner FROM MENUS WHERE active = 1 AND user IS NOT NULL";
-
-    $query = mysqli_query($conection, $sentence) or die(ERROR_CONSULTA_DB);
-
-    while($choice = mysqli_fetch_array($query)) {
-    	if(! ($choice['sunday_dinner'] == "" && $choice['sunday_lunch'] == "") ) {
-    		$pdf->Write(14, "" . $choice['user'] . ":\t\t" . $choice['sunday_lunch'] . "\t\t-\t\t" . $choice['sunday_dinner'] . "\n");    		
-    	}
-    }
 
 	$pdf->Output();
+
+
+	function printDay($day, $dia) {
+		global $pdf, $conection;
+		$pdf->SetFont('Arial', 'B', 16);
+        $str = iconv('UTF-8', 'windows-1252', "\n" . $dia . "\n");
+        $pdf->Write(16, $str);
+		$pdf->SetFont('Arial','', 16);
+
+		$lunch = "" . $day . "_lunch";
+		$dinner = "" . $day . "_dinner";
+		
+	    $sentence = "SELECT user, " . $lunch . ", " . $dinner . " FROM MENUS WHERE active = 1 AND user IS NOT NULL";
+
+	    $query = mysqli_query($conection, $sentence) or die(ERROR_CONSULTA_DB);
+
+	    while($choice = mysqli_fetch_array($query)) {
+	    	if(! ($choice[$dinner] == "" && $choice[$lunch] == "") ) {
+			    $str1 = iconv('UTF-8', 'windows-1252', $choice[$lunch]);   		
+			    $str2 = iconv('UTF-8', 'windows-1252', $choice[$dinner]);
+
+	    		$pdf->Write(14, "" . $choice['user'] . ":\t\t" . $str1 . "\t\t-\t\t" . $str2 . "\n");    
+	    	}
+	    }
+
+	}
 ?>
