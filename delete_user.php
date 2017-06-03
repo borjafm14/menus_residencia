a7d7824329e1062d8095222e585e77bad827da0c
Borja Fernandez (uniLeon.Inf.TFG2016-17.MagoBestias) <bfernm04@estudiantes.unileon.es>
2017-06-03 02:10:34 +0200

    Update

--- delete_user.php
+++ delete_user.php
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream
 <?php
 
   /* Comienzo de la sesion*/
@@ -26,32 +25,3 @@
   /* Libera el resultado de la consulta SQL. */
   mysqli_free_result($query);
 
-=======
-<?php
-
-  /* Comienzo de la sesion*/
-  session_start();
-
-  Include('connector_db.php');
-
-
-
-   $usuario = $_GET['user'];
-
-
-  /* Variable global para almacenar la query*/
-  $query;
-
-  $conection = establecerConexionDB();
-
-  $sentence = "DELETE FROM USERS WHERE user='$usuario'";
-
-  /* Ejecucion de la sentencia */
-  $query = mysqli_query($conection, $sentence) or die(ERROR_CONSULTA_DB);
-
-  header('Location: admin.php?type=users');
-
-  /* Libera el resultado de la consulta SQL. */
-  mysqli_free_result($query);
-
->>>>>>> Stashed changes
--- js/admin.js
+++ js/admin.js
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream
 //para que funcionen los modal
 $(document).ready(function(){
 	$('#modal_menu').modal();	
@@ -55,62 +54,4 @@ function validateIncrement(){
 	}
 
 	return true;
-=======
-//para que funcionen los modal
-$(document).ready(function(){
-	$('#modal_menu').modal();	
-	$('#modal_ingredient').modal();
-	$('#modal_user').modal();
-});
-
-
-function validateIngredient(){
-	var ingredient, quantity;
-
-	ingredient = document.forms["form_ingredient"]["nombre_ingrediente"].value;
-	quantity = document.forms["form_ingredient"]["cantidad_inicial"].value;
-	
-	if (ingredient == "" || quantity == "") {
-
-	  Materialize.toast("Se deben rellenar todos los campos", 3000, "light-green darken-1");
-	  return false;
-	}
-
-	return true;
-}
-
-
-function validateUser(){
-	var nombre_usuario, pass_usuario, nombre, apellido, email, direccion_email, tipo_usuario;
-
-	nombre_usuario = document.forms["form_user"]["nombre_usuario"].value;
-	pass_usuario = document.forms["form_user"]["pass_usuario"].value;
-	nombre = document.forms["form_user"]["nombre"].value;
-	apellido = document.forms["form_user"]["apellido"].value;
-	direccion_email = document.forms["form_user"]["direccion_email"].value;
-	tipo_usuario = document.forms["form_user"]["tipo_usuario"].value;
-
-
-	
-	if (nombre_usuario == "" || pass_usuario == "" || nombre == "" || apellido == "" || direccion_email == "" || tipo_usuario == "") {
-
-	  Materialize.toast("Se deben rellenar todos los campos", 3000, "light-green darken-1");
-	  return false;
-	}
-
-	return true;
-}
-
-
-function validateIncrement(){
-	var increment;
-
-	increment = document.forms["form_increment"]["quantity"].value;
-	
-	if (increment == "") {
-	  return false;
-	}
-
-	return true;
->>>>>>> Stashed changes
 }
