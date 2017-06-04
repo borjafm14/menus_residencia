3cea06ce33672d48738b84f7dd3748010605e700
Dafer <aferns08@estudiantes.unileon.es>
2017-06-04 11:51:01 +0200

    Reports update
    Little changes on pdf output

--- generador_informes.php
+++ generador_informes.php
@@ -3,12 +3,25 @@
   	session_start();
   	Include('connector_db.php');
 	require('fpdf/fpdf.php');
-    
+
   	$conection = establecerConexionDB();
 
 	$pdf = new FPDF();
 	$pdf->AddPage();
 
+    $sentence = "SELECT fecha_hora FROM MENU_FORMS WHERE active = 1";
+    $query = mysqli_query($conection, $sentence) or die(ERROR_CONSULTA_DB);
+    $result = mysqli_fetch_array($query);
+    
+    $pdf->SetFont('Arial', 'BU', 30);
+    $str = iconv('UTF-8', 'windows-1252', "Menú semanal");
+    $pdf->Cell(95,20, $str);
+    $pdf->SetFont('Arial', '', 2);
+
+    $pdf->SetFont('Arial', '', 12);
+    $str = iconv('UTF-8', 'windows-1252', "Fecha de creación del menú: ");
+    $str = $str . $result['fecha_hora'];
+    $pdf->Cell(400,20, $str);
     
     printDay("monday", "Lunes");
     printDay("tuesday", "Martes");
@@ -18,21 +31,18 @@
 	printDay("saturday", "Sábado");
     printDay("sunday", "Domingo");
 
-
 	$pdf->Output();
-
 
 	function printDay($day, $dia) {
 		global $pdf, $conection;
-		$pdf->SetFont('Arial', 'B', 16);
+		$pdf->SetFont('Arial', 'BU', 18);
         $str = iconv('UTF-8', 'windows-1252', "\n" . $dia . "\n");
         $pdf->Write(16, $str);
-		$pdf->SetFont('Arial','', 16);
 
 		$lunch = "" . $day . "_lunch";
 		$dinner = "" . $day . "_dinner";
 		
-	    $sentence = "SELECT user, " . $lunch . ", " . $dinner . " FROM MENUS WHERE active = 1 AND user IS NOT NULL";
+	    $sentence = "SELECT user, " . $lunch . ", " . $dinner . " FROM MENUS WHERE active = 1 AND user IS NOT NULL ORDER BY user ASC";
 
 	    $query = mysqli_query($conection, $sentence) or die(ERROR_CONSULTA_DB);
 
@@ -41,7 +51,10 @@
 			    $str1 = iconv('UTF-8', 'windows-1252', $choice[$lunch]);   		
 			    $str2 = iconv('UTF-8', 'windows-1252', $choice[$dinner]);
 
-	    		$pdf->Write(14, "" . $choice['user'] . ":\t\t" . $str1 . "\t\t-\t\t" . $str2 . "\n");    
+                $pdf->SetFont('Arial','B', 13);
+                $pdf->Cell(40,14, "" . $choice['user'] . ": \t\t");
+                $pdf->SetFont('Arial','', 13);
+	    		$pdf->Write(14, "" . $str1 . "\t\t-\t\t" . $str2 . "\n");    
 	    	}
 	    }
 
